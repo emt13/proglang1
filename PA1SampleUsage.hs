120fbb68b4fee8901fe44de5f5c66c20c0a78d25
mwdewey <mwdewey@optonline.net>
2015-09-23 20:06:36 -0400

    commit

--- PA1SampleUsage.hs
+++ PA1SampleUsage.hs
@@ -1,4 +1,5 @@
 import PA1Helper
+import Debug.Trace
 
 -- Haskell representation of lambda expression
 -- In Lambda Lexp Lexp, the first Lexp should always be Atom String
@@ -20,10 +21,26 @@ id' v@(Atom _) = v
 id' lexp@(Lambda (Atom _) _) = lexp
 id' lexp@(Apply _ _) = lexp 
 
+
+red :: Lexp -> Lexp
+--red lexp@(Lambda e m) = trace ("L e: " ++ (show e) ++ " x: " ++ (show m)) beta ((red e) (red m))
+--red lexp@(Apply e m) = trace ("A e: " ++ (show e) ++ " x: " ++ (show m)) beta ((red e) (red m))
+red lexp@(Lambda e m) = trace ("L e: " ++ (show e) ++ " x: " ++ (show m)) beta lexp
+red lexp@(Apply e m) = trace ("A e: " ++ (show e) ++ " x: " ++ (show m)) beta lexp
+red v@(Atom _) = v
+
+beta :: Lexp -> Lexp
+beta (Apply (Lambda a b) c) 
+  | a == b = c
+  | otherwise = b
+  
+  
+ 
+
 -- Entry point of program
 main = do
     putStrLn "Please enter a filename containing lambda expressions:"
     fileName <- getLine
     -- id' simply returns its input, so runProgram will result
     -- in printing each lambda expression twice. 
-    runProgram fileName id'  
+    runProgram fileName red 
